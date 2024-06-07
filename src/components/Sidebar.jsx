cd8ffb02a9d4387a8a2a30eda20f07417af074bb
anderrf <anderson_rocha_farias@outlook.com>
2024-06-07 17:27:14 -0300

    avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import styles from './Sidebar.module.css'
 import {PencilLine} from 'phosphor-react'
 
@@ -8,7 +9,7 @@ export function Sidebar(){
                 src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?q=50&w=500" 
             />
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/anderrf.png"/>
+                <Avatar src="https://github.com/anderrf.png"/>
                 <strong>Anderson Farias</strong>
                 <span>Web Developer</span>
             </div>
