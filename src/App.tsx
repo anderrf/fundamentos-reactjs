1b248a1cb23adb06914477d43e3ea3622300e4eb
anderrf <anderson_rocha_farias@outlook.com>
2024-06-10 17:33:50 -0300

    addition of Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import {Post} from './components/Post'
+import {Post, PostType} from './components/Post'
 import { Header } from './components/Header'
 import './global.css'
 import styles from './App.module.css'
 import { Sidebar } from './components/Sidebar'
+import {} from './components/Post'
 
-const posts = [
+
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -50,9 +52,7 @@ export function App() {
               return (
                 <Post 
                   key={post.id}
-                  author={post.author}
-                  content={post.content}
-                  publishedAt={post.publishedAt}
+                  post={post}
                 />
               )
             })
